132bab10675ea8d1b700a18caad37b1c38383c8f
Tom Chen <developer@tomchentw.com>
2014-06-25 21:01:36 +0800

    refactor(index.js): changed to nodejs styled callback

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ module.exports = function(opt) {
 		/*jshint validthis:true */
 
 		if (file.isNull()) {
-			this.push(file);
-			return callback();
+			return callback(null, file);
 		}
 
 		if (file.isStream()) {
@@ -65,9 +64,7 @@ module.exports = function(opt) {
 			file.sourceMap.sources = originalSourceMap.sources;
 		}
 
-		this.push(file);
-
-		callback();
+		callback(null, file);
 	}
 
 	return through.obj(minify);
