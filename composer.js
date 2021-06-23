db577796101674ec8956e7dc2768065b3618563b
Mathew Heard <splitice@users.noreply.github.com>
2021-06-23 12:50:11 +1000

    support asyncronous minifiers (e.g new terser)

--- composer.js
+++ composer.js
@@ -5,11 +5,11 @@ var minify = require('./lib/minify');
 module.exports = function(uglify, logger) {
   return function(opts) {
     var minifier = minify(uglify, logger)(opts);
-    return through.obj(function(file, encoding, callback) {
+    return through.obj(async function(file, encoding, callback) {
       var newFile = null;
       var err = null;
       try {
-        newFile = minifier(file);
+        newFile = await minifier(file);
       } catch (e) {
         err = e;
       }
